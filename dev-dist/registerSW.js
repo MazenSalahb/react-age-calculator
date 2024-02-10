af6bba9a84669a6a172bcb793fac631161eca07b
Mazen Salah <mazensalah360@gmail.com>
2024-02-10 23:26:31 +0200

    pwa

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/react-age-calculator/dev-sw.js?dev-sw', { scope: '/react-age-calculator/', type: 'classic' })
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/react-age-calculator",
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        publicPath: "/react-age-calculator/",
+        name: "Age calculator",
+        description: "an app that calculate your age.",
+        icons: [
+          {
+            src: "/react-age-calculator/favicon.png", // Changed the path to the icon
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      registerType: "prompt",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
